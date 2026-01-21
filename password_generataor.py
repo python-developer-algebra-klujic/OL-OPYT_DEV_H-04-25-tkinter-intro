f0c7f68be273aea5844f88b19fc876644649ad39
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2026-01-21 20:54:54 +0100

    Update password_generataor.py

--- password_generataor.py
+++ password_generataor.py
@@ -1,20 +1,38 @@
 import random
-# svi znakovi su od 33 - 126
+import tkinter as tk
 
 
-password_length = 15
-characters = [chr(i) for i in range(33, 127)]
-random.shuffle(characters)
-password = ''
-for letter in characters[ : password_length]:
-    password += letter
+PWD_LENGHT = 15
 
-print(password)
+def password_generator():
+    password = ''
+    characters = [chr(i) for i in range(33, 127)]
+    random.shuffle(characters)
 
-'''
-PASWORD GENERATOR
+    for letter in characters[ : PWD_LENGHT]:
+        password += letter
+    lbl_password_var.set(password)
 
-PASSWORD VALUE - default value '!Pa$$w0rd!'
 
-[generate password]
-'''
+root = tk.Tk()
+root.title('Password Generator')
+
+lbl_title = tk.Label(root,
+                     text='Python Generator',
+                     font=('Verdana', 20))
+lbl_title.pack(padx=10, pady=15)
+
+lbl_password_var = tk.StringVar(root, '!Pa$$w0rd!')
+lbl_password = tk.Label(root,
+                        textvariable=lbl_password_var,
+                        font=('Verdana', 25))
+lbl_password.pack(padx=10, pady=15)
+
+btn_generate_pwd = tk.Button(root,
+                             text='generate password',
+                             font=('Verdana', 18),
+                             command=password_generator)
+btn_generate_pwd.pack(padx=10, pady=15)
+
+
+root.mainloop()
