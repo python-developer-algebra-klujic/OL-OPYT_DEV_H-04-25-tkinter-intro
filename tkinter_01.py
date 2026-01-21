159a29100c522b1fc959c6004c07e309ed6576a8
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2026-01-21 20:20:21 +0100

    Update tkinter_01.py

--- tkinter_01.py
+++ tkinter_01.py
@@ -11,34 +11,27 @@ def decrement():
 
 root = tk.Tk()
 root.title('TkInter DEMO')
-root.geometry('600x400')
+# root.geometry('600x400')
 
 lbl_title = tk.Label(root,
-                     text='Pozdrav iz TkIntera!',
+                     text='Py Counter',
                      font=('Verdana', 18))
 lbl_title.pack(padx=10, pady=10)
-lbl_subtitle = tk.Label(root,
-                        text='Ovo je moja prva TkInter aplikacija',
-                        font=('Verdana', 14))
-lbl_subtitle.pack(padx=10, pady=10)
-
-
-btn_increment = tk.Button(root,
-                          text='Increment (+)',
-                          font=('Verdana', 14),
-                          command=increment)
-btn_increment.pack(padx=10, pady=10)
-btn_decrement = tk.Button(root,
-                          text='Decrement (-)',
-                          font=('Verdana', 14),
-                          command=decrement)
-btn_decrement.pack(padx=10, pady=10)
 
 lbl_counter_var = tk.DoubleVar(root, 0)
-lbl_counter = tk.Label(root,
-                       textvariable=lbl_counter_var,
-                       font=('Verdana', 25))
-lbl_counter.pack(padx=10, pady=10)
-
+btn_increment = tk.Button(root,
+                          textvariable=lbl_counter_var,
+                          font=('Verdana', 24),
+                          background='#32CD32',
+                          width=10, height=5,
+                          command=increment)
+btn_increment.pack(padx=20, pady=20, side=tk.LEFT)
+btn_decrement = tk.Button(root,
+                          textvariable=lbl_counter_var,
+                          font=('Verdana', 24),
+                          background='#C30f16',
+                          width=10, height=5,
+                          command=decrement)
+btn_decrement.pack(padx=20, pady=20, side=tk.RIGHT)
 
 root.mainloop()
