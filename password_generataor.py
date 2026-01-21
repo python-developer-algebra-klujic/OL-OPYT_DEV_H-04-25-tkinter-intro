837191ccca1513e09b9af435cdf0cd27a051ce9d
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2026-01-21 20:30:03 +0100

    Create password_generataor.py

---
+++ password_generataor.py
@@ -0,0 +1,12 @@
+import random
+# svi znakovi su od 33 - 126
+
+
+password_length = 15
+characters = [chr(i) for i in range(33, 127)]
+random.shuffle(characters)
+password = ''
+for letter in characters[ : password_length]:
+    password += letter
+
+print(password)
